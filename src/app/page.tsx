6d7ed7b775555d59e983828a1a683f75a16641ee
Guilherme Vianna <mataveli91@gmail.com>
2023-11-10 15:58:33 -0300

    main page parcial added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import BUTTON_TYPE_1 from "@/components/BUTTON_TYPE_1";
+
 export default function Home() {
   return (
-   <></>
+    <div className="h-screen w-full backgroudSkyBlue">
+      <div className="mx-5 flex flex-row">
+        <div className="text-7xl">
+          lesson-jm
+        </div>
+        <BUTTON_TYPE_1 text="Registrar"/>
+      </div>
+    </div>
   )
 }
