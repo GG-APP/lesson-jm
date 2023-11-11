8c314137d60efb8c622c67e1b351471f3967168d
Guilherme Vianna <59426923+Guilherme-Vianna@users.noreply.github.com>
2023-11-11 16:08:23 -0300

    home screen added

--- src/components/BUTTON_TYPE_1.tsx
+++ src/components/BUTTON_TYPE_1.tsx
@@ -1,10 +1,10 @@
 interface BUTTON_TYPE_1_PROPS {
-  text : string 
+  text: string
 }
-export default function BUTTON_TYPE_1 (props: BUTTON_TYPE_1_PROPS) { 
-  return ( 
-    <div className="w-14 h-14 contrastSkyBlue">
-      <div className="">
+export default function BUTTON_TYPE_1(props: BUTTON_TYPE_1_PROPS) {
+  return (
+    <div className="flex justify-center items-center w-40 h-14 rounded-full duration-300 button_type_1 hover:shadow-xl">
+      <div className="text-center text-2xl">
         {props.text}
       </div>
     </div>
