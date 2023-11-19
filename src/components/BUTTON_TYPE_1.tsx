7eec5079e703d2b3d0aecf35237b9677e4f93318
Guilherme Vianna <59426923+Guilherme-Vianna@users.noreply.github.com>
2023-11-18 22:03:40 -0300

    nomenclature changed

--- src/components/BUTTON_TYPE_1.tsx
+++ src/components/BUTTON_TYPE_1.tsx
@@ -1,9 +1,9 @@
-interface BUTTON_TYPE_1_PROPS {
+interface BUTTON_TYPE_1 {
   text: string
   functionTrigger?: () => void
 }
 
-export default function BUTTON_TYPE_1(props: BUTTON_TYPE_1_PROPS) {
+export default function BUTTON_TYPE_1(props: BUTTON_TYPE_1) {
   return (
     <button onClick={props.functionTrigger}>
       <div className="flex justify-center items-center w-40 h-14 rounded-full transition duration-300 hover:rounded-xl button_type_1 hover:shadow-xl">
